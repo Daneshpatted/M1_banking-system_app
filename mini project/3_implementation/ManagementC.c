d081fe44aa8bd774d9f6e274f472b7b2ce9da2bb
Daneshpatted <98831772+Daneshpatted@users.noreply.github.com>
2022-02-11 21:59:37 -0800

    Update ManagementC.c

--- mini project/3_implementation/ManagementC.c
+++ mini project/3_implementation/ManagementC.c
@@ -32,11 +32,11 @@ int main()
 
 main:
 
-    //system("cls");    /* ************Main menu ***********************  */
+    //system("cls");    /* ****Main menu ****  */
 
     printf("\n\t **** Welcome to Contact Management System ****");
 
-    printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
+    printf("\n\n\n\t\t\tMAIN MENU\n\t\t======\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
 
     printf("Enter the choice:");
 
@@ -52,7 +52,7 @@ main:
 
         break;
 
-        /* *********************Add new contacts************  */
+        /* *****Add new contacts******  */
 
     case 1:
 
@@ -111,7 +111,7 @@ main:
 
         //system("cls");
 
-        printf("\n\t\t================================\n\t\t\tLIST OF CONTACTS\n\t\t================================\n\nName\t\tPhone No\t    Address\t\tE-mail ad.\n=================================================================\n\n");
+        printf("\n\t\t=============\n\t\t\tLIST OF CONTACTS\n\t\t===============\n\nName\t\tPhone No\t    Address\t\tE-mail ad.\n==============\n\n");
 
         for(i=97; i<=122; i=i+1)
 
@@ -145,7 +145,7 @@ main:
             if(found!=0)
 
             {
-                printf("=========================================================== [%c]-(%d)\n\n",i-32,found);
+                printf("======== [%c]-(%d)\n\n",i-32,found);
 
                 getchar();
             }
@@ -155,8 +155,8 @@ main:
         }
 
         break;
-
-        /* *******************search contacts**********************  */
+            
+        /* ********search contacts******  */
 
     case 3:
 
@@ -168,7 +168,7 @@ main:
 
             found=0;
 
-            printf("\n\n\t..::CONTACT SEARCH\n\t===========================\n\t..::Name of contact to search: ");
+            printf("\n\n\t..::CONTACT SEARCH\n\t=============\n\t..::Name of contact to search: ");
 
             fflush(stdin);
 
@@ -180,7 +180,7 @@ main:
 
             system("cls");
 
-            printf("\n\n..::Search result for '%s' \n===================================================\n",query);
+            printf("\n\n..::Search result for '%s' \n==================\n",query);
 
             (fread(&list,sizeof(list),1,fp)==1);
 
@@ -247,7 +247,7 @@ main:
 
         fflush(stdin);
 
-        printf("..::Edit contact\n===============================\n\n\t..::Enter the name of contact to edit:");
+        printf("..::Edit contact\n============\n\n\t..::Enter the name of contact to edit:");
 
         scanf("%[^\n]",name);
 
@@ -303,7 +303,7 @@ main:
 
         break;
 
-        /* ********************delete contacts**********************/
+        /* **************delete contacts*************/
 
     case 5:
 
@@ -311,7 +311,7 @@ main:
 
         fflush(stdin);
 
-        printf("\n\n\t..::DELETE A CONTACT\n\t==========================\n\t..::Enter the name of contact to delete:");
+        printf("\n\n\t..::DELETE A CONTACT\n\t===========\n\t..::Enter the name of contact to delete:");
 
         scanf("%s",&name);
 
